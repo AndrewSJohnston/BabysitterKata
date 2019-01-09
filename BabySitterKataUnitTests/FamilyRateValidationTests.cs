a71f2a3c2b771170c065d230ef55f0449de112b8
Andrew Johnston <CynanApMachaeXbox360@hotmail.com>
2019-01-09 16:58:04 -0500

    Set up Test and rates for Family A

---
+++ BabySitterKataUnitTests/FamilyRateValidationTests.cs
@@ -0,0 +1,25 @@
+﻿using BabysitterKata;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace BabySitterKataUnitTests
+{
+    [TestClass]
+    public class FamilyRateValidationTests
+    {
+        [TestMethod]
+        public void FamilyARateValidation()
+        {
+            var familyARates = new FamilyARateRangeList();
+
+            //Make sure all the rate ranges match the spec
+            Assert.IsTrue(familyARates.FamilyRateRanges.All(a => (a.RateInDollars == 15 
+                                                                    && a.TimeRangeForRate.StartingTime24 == 17 
+                                                                    && a.TimeRangeForRate.EndingTime24H == 23)
+                                                                || (a.RateInDollars == 20
+                                                                    && a.TimeRangeForRate.StartingTime24 == 23
+                                                                    && a.TimeRangeForRate.EndingTime24H == 4)));
+        }
+    }
+}
--- BabysitterKata/ScheduleFamilyRates.cs
+++ BabysitterKata/ScheduleFamilyRates.cs
@@ -10,13 +10,75 @@ namespace BabysitterKata
         public int RateInDollars;
     }
 
-    public class FamilyARateRangeList
+     public class FamilyRateRangeList
     {
-        private List<ScheduleIntervalRate> _familyRateRanges;
+        internal List<ScheduleIntervalRate> _familyRateRanges;
+
+        public List<ScheduleIntervalRate> FamilyRateRanges
+        {
+            get { return _familyRateRanges; }
+        }
+    }
+
+
+    public class FamilyARateRangeList : FamilyRateRangeList
+    {
 
         public FamilyARateRangeList()
         {
-            _familyRateRanges = new List<ScheduleIntervalRate>();
+            _familyRateRanges = new List<ScheduleIntervalRate>()
+            {
+                //Family A pays $15 per hour before 11pm
+                new ScheduleIntervalRate()
+                {
+                    RateInDollars = 15,
+                    TimeRangeForRate = new ScheduledInterval()
+                    {
+                        EndingTime24H = 23,
+                    }
+                },
+
+                //Family A pays $20 per hour the rest of the night
+                new ScheduleIntervalRate()
+                {
+                    RateInDollars = 20,
+                    TimeRangeForRate = new ScheduledInterval()
+                    {
+                        StartingTime24 = 23
+                    }
+                },
+            };
+        }
+    }
+
+    public class FamilyBRateRangeList
+    {
+        private List<ScheduleIntervalRate> _familyRateRanges;
+
+        public FamilyBRateRangeList()
+        {
+            _familyRateRanges = new List<ScheduleIntervalRate>()
+            {
+                //Family A pays $15 per hour before 11pm
+                new ScheduleIntervalRate()
+                {
+                    RateInDollars = 15,
+                    TimeRangeForRate = new ScheduledInterval()
+                    {
+                        EndingTime24H = 23,
+                    }
+                },
+
+                //Family A pays $20 per hour the rest of the night
+                new ScheduleIntervalRate()
+                {
+                    RateInDollars = 20,
+                    TimeRangeForRate = new ScheduledInterval()
+                    {
+                        StartingTime24 = 23
+                    }
+                },
+            };
         }
 
         public List<ScheduleIntervalRate> FamilyRateRanges
