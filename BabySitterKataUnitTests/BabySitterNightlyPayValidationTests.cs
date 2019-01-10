427ec2d00277492f611bac76524693a4c7de2529
Andrew Johnston <CynanApMachaeXbox360@hotmail.com>
2019-01-10 08:34:31 -0500

    Stubbed out pay calculation.

---
+++ BabySitterKataUnitTests/BabySitterNightlyPayValidationTests.cs
@@ -0,0 +1,20 @@
+﻿using BabysitterKata;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace BabySitterKataUnitTests
+{
+    [TestClass]
+    public class BabySitterNightlyPayValidationTests
+    {
+        [TestMethod]
+        public void FamilyAFullNightRateTest()
+        {
+
+            var nightlyPay = NightlyTotalPayCalculator.CalculateAmountOwedForGivenNightlySchedule(new ScheduledInterval(), new FamilyARateRangeList());
+
+            Assert.IsTrue(nightlyPay == 0);
+        }
+    }
+}
---
+++ BabysitterKata/NightlyTotalPayCalculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BabysitterKata
+{
+    public static class NightlyTotalPayCalculator
+    {
+        static public int CalculateAmountOwedForGivenNightlySchedule(ScheduledInterval BabysittingSchedule, FamilyRateRangeList givenFamilyRates)
+        {
+            return 0;
+        }
+    }
+}
