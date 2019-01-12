a1e70ed14e673ca85d1bb1281070be298364d5fb
Andrew Johnston <CynanApMachaeXbox360@hotmail.com>
2019-01-12 09:34:18 -0500

    Improved input coverage.

--- BabySitterKataUnitTests/BabysitterScheduleTimeValidationTests.cs
+++ BabySitterKataUnitTests/BabysitterScheduleTimeValidationTests.cs
@@ -1,48 +1,83 @@
 using BabysitterKata;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 
 namespace BabySitterKataUnitTests
 {
     [TestClass]
     public class BabysitterScheduleTimeValidationTests
     {
+        List<int> validStartTimes = new List<int>();
+        List<int> validEndTimes = new List<int>();
+
+        [TestInitialize()]
+        public void Setup()
+        {
+            for (var x = 17; x <= 24; x++)
+            {
+                validStartTimes.Add(x);
+            }
+
+            for (var x = 1; x <= 3; x++)
+            {
+                validStartTimes.Add(x);
+            }
+
+            for (var x = 18; x <= 24; x++)
+            {
+                validEndTimes.Add(x);
+            }
+
+            for (var x = 1; x <= 4; x++)
+            {
+                validEndTimes.Add(x);
+            }
+        }
+
         #region Start and End Time Validation
 
         #region Start Time Validation
 
-        [TestMethod]
-        public void NightScheduleAcceptsValidStartingTime()
+        public void TestTimeRange(int x, bool isStartTime = false, bool testIfValid = false)
         {
             try
             {
                 var schedule = new ScheduledInterval()
                 {
-                    StartingTime24H = 17,
+                    StartingTime24H = isStartTime ? x : 17,
+                    EndingTime24H = !isStartTime ? x : 4,
                 };
             }
             catch (Exception e)
             {
+                if (testIfValid)
+                    Assert.Fail();
+                else
+                    return;
+            }
+
+            if (!testIfValid)
                 Assert.Fail();
+        }
+
+        [TestMethod]
+        public void NightScheduleAcceptsValidStartingTime()
+        {
+            foreach (var validValue in validStartTimes)
+            {
+                TestTimeRange(validValue, true, true);
             }
         }
 
         [TestMethod]
         public void NightScheduleFailsWithInvalidStartingTime()
         {
-            try
+            for (var x = -48; x <= 48; x++)
             {
-                var schedule = new ScheduledInterval()
-                {
-                    StartingTime24H = 12,
-                };
+                if (!validStartTimes.Contains(x))
+                    TestTimeRange(x, true);
             }
-            catch (Exception e)
-            {
-               return;
-            }
-
-            Assert.Fail();
         }
 
         #endregion
@@ -52,34 +87,20 @@ namespace BabySitterKataUnitTests
         [TestMethod]
         public void NightScheduleAcceptsValidEndingTime()
         {
-            try
+            foreach (var validValue in validEndTimes)
             {
-                var schedule = new ScheduledInterval()
-                {
-                    EndingTime24H = 18,
-                };
-            }
-            catch (Exception e)
-            {
-                Assert.Fail();
+                TestTimeRange(validValue, false, true);
             }
         }
 
         [TestMethod]
         public void NightScheduleFailsWithInvalidEndingTime()
         {
-            try
+            for (var x = -48; x <= 48; x++)
             {
-                var schedule = new ScheduledInterval()
-                {
-                    EndingTime24H = 12,
-                };
+                if (!validEndTimes.Contains(x))
+                    TestTimeRange(x);
             }
-            catch (Exception e)
-            {
-                return;
-            }
-            Assert.Fail();
         }
         #endregion
     }
